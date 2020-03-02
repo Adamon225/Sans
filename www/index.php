1fabffdc635170572ca9ee26667a718304455c4f
Adamon225 <43549885+Adamon225@users.noreply.github.com>
2020-03-02 20:00:03 +0100

    Add files via upload

---
+++ www/index.php
@@ -0,0 +1,73 @@
+ <!DOCTYPE HTML>
+ <html lang="pl">
+ <head>
+ <meta charset= "utf-8"/>
+ <title>Sans biblioteka</title>
+ <meta name= "describtion" content="Biblioteka Sansów"/>
+ <meta name="keywords" content="Sans, undertale"/>
+ <meta http-equiv="X-UA-Compatible" content="IE= edge,chrome=1"/>
+ <link rel="stylesheet" href="style.css" type="text/css"/>
+
+ </head>
+ 
+<body>
+ <div id= "Container">
+ <div id= "Logo">
+ <h1>SANS</h1>
+ <?php
+ $imie_i_nazwisko="Adam Fortecki";
+ echo '<h1>$imie_i_nazwisko'.",witaj na stronie $imie_i_nazwisko</h1>";
+ ?>
+ </div>
+ <div id= "Names">
+ <div class="obrazek">
+  <div class="pustka">
+ </div>
+ Sans <br/>
+ </div>
+ <div class="obrazek">
+ <div class="pustka">
+ </div>
+<a href="https://gamejolt.com/games/ildiodeigiochicom/309864/download/build/780552" target="_blank" title="pobierz ink sansa">
+ Ink Sans <br/>
+ </a>
+ </div>
+ <div class="obrazek">
+  <div class="pustka">
+ </div>
+ <a href="https://gamejolt.com/games/UltraSans/243436/download/build/439147"target="_blank" title="pobierz ultra sansa">
+ Ultra Sans<br/>
+ </a>
+ </div>
+ <div class="obrazek">
+  <div class="pustka">
+ </div>
+ <a href="https://pl.y8.com/games/underfell_-_sans_battle" title="bitwa z Underfell sansem online" target="_blank">
+ Underfell Sans <br/>
+ </a>
+ </div>
+ </div>
+ <div id="Look">
+ <img  width="189px" height="182px" src="img/Sans.jpg"/><br/>
+<a href="https://gamejolt.com/games/ildiodeigiochicom/309864/download/build/780552" target="_blank" title="pobierz ink sansa">
+<img width="325px" height="182px" src="img/ink sans.jpg"/><br/>
+</a>
+<a href="https://gamejolt.com/games/UltraSans/243436/download/build/439147"target="_blank" title="pobierz ultra sansa">
+<img width="325px" height="182px" src="img/ultra sans.jpg"/>
+</a>
+<br/>
+<a href="https://pl.y8.com/games/underfell_-_sans_battle" title="bitwa z Underfell sansem online" target="_blank">
+ <img width="325px" height="182px"src="img/Underfell.jpg"/>
+ </a>
+ </div>
+ <div id="OtherLibs">
+ flowey biblioteka
+ 
+ </div>
+ <div id ="Footer">
+ 4rt3trtt3
+ 
+ </div>
+ </div>
+</body> 
+ </html>
